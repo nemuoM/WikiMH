18eced6fd9d95e4d006880918394a834a69ed1d2
Nemuom <abdelmoumen.morabet@gmail.com>
2025-02-13 17:26:22 +0100

    fix: ajout de la documentation

--- model/DbManager.php
+++ model/DbManager.php
@@ -17,6 +17,29 @@
  class DbManager{
     private static ?\PDO $cnx = null;
 
+    /**
+     * Obtient une connexion à la base de données.
+     *
+     * Cette méthode utilise le patron Singleton pour s'assurer qu'une seule instance
+     * de connexion à la base de données est créée. Si la connexion n'existe pas déjà,
+     * elle est créée en utilisant les informations de configuration spécifiées.
+     *
+     * @return PDO L'objet PDO représentant la connexion à la base de données.
+     *
+     * @throws PDOException Si une erreur survient lors de la connexion à la base de données.
+     *
+     * @example
+     * ```php
+     * \$connexion = MaClasse::getConnexion();
+     * ```
+     *
+     * @global string HOST L'hôte de la base de données.
+     * @global string PORT Le port de la base de données.
+     * @global string DBNAME Le nom de la base de données.
+     * @global string CHARSET Le jeu de caractères utilisé pour la connexion.
+     * @global string LOGIN Le nom d'utilisateur pour la connexion à la base de données.
+     * @global string MDP Le mot de passe pour la connexion à la base de données.
+     */
     public static function getConnexion() {
         if (self::$cnx == null) {
             try {
