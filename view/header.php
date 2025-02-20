a1ec6a53fc014c7d751950f9d26bf87420cc76cc
Lemonraptor <147720019+Lemonraptor@users.noreply.github.com>
2025-02-20 15:34:50 +0100

    Update header.php
    add header

---
+++ view/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wiki Monster Hunter</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/home.css">
+</head>
+<body>
+    <header>
+        <h1>Wiki Monster Hunter</h1>
+    </header>
+    <nav>
+    <a href="/">Accueil</a>
+    <a href="#monstres">Monstres</a>
+    <a href="#armes">Armes</a>
+    <a href="#armures">Armures</a>
+    <a href="#items">Items</a>
+    <a href="#favoris">Favoris</a>
+    </nav>
