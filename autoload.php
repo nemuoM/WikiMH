ce9462b1e895cb5d69ac4e664ff2060f7c635c67
Nemuom <abdelmoumen.morabet@gmail.com>
2025-02-21 09:45:42 +0100

    feat: affichage de la page Home

---
+++ autoload.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * /autoload.php
+ * Charge les classes
+ * 
+ * @author Moumen
+ * 02/2025
+ */
+
+require_once ROOT . '/model/DbManager.php';
+require_once ROOT . '/controller/Controller.php';
---
+++ controller/ViewController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * /controller/ViewController.php
+ * 
+ * Contrôleur pour les pages de vue uniquement
+ *
+ * @author Moumen
+ * @date 02/2025
+ */
+
+class ViewController extends Controller
+{
+
+    /**
+     * Action qui affiche la page d'accueil
+     */
+    public static function home($params)
+    {
+        $view = ROOT . '/view/home.php';
+        $params = array();
+        self::render($view, $params);
+    }
+}
